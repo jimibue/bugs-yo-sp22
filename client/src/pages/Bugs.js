8f2d43c51459e5fa3e506f5cfd23e2f14eb306d7
James <jimibue@gmail.com>
2022-03-09 13:31:53 -0800

    axios

--- client/src/pages/Bugs.js
+++ client/src/pages/Bugs.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Bugs = ()=>{
+    useEffect(()=>{
+        getBugs()
+    },[])
+    const getBugs = async()=>{
+        let res = await axios.get('/api/bugs')
+        setBugs(res.data)
+    }
+    const [bugs, setBugs] = useState([])
     return (
         <div>
             <h1>Bugs</h1>
+            <p>{JSON.stringify(bugs)}</p>
         </div>
     )
 }
